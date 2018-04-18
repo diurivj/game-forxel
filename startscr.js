ef0c3cb3c45c164aa950d538f35452cb109404b7
Diego Vazquez <diurivj@icloud.com>
2018-04-18 09:53:31 -0500

    wrong png?

--- startscr.js
+++ startscr.js
@@ -25,12 +25,17 @@ function BoardStart(imgSrc){
   this.height     = canvas.height;
   this.img        = new Image();
   this.img.src    = imgSrc || images.startImage;
+
+  this.img.onload = function(){
+    this.draw();
+  }.bind(this);
+
   this.draw = function(){
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
   }  
 };
 
-var inicio  = new BoardStart();
+var inicio  = new BoardStart(images.startImage);
 var selec1  = new BoardStart(images.player1);
 var selec2  = new BoardStart(images.player2);
 var count   = new BoardStart(images.field);
